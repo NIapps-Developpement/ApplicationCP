4ef1771a09066e4db7c0d47b9a539f88b78580af
Haladeen <nathan.rossler@skynet.be>
2018-07-01 18:06:41 +0200

    Push accueil 1

--- app/src/main/java/be/cerclepolytechnique/applicationcp/MainActivity.java
+++ app/src/main/java/be/cerclepolytechnique/applicationcp/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         GetNews();
+        navigation.setItemIconTintList(null);
 
 
 
